e51f6a264f5546a31d436fc73252a1a7177b1a42
darkside-86 <ismail.ax.2011@gmail.com>
2019-03-08 13:03:04 -0500

    Huge code reorganizing

--- configure.bat
+++ configure.bat
@@ -1,4 +1,4 @@
 IF NOT EXIST build ( mkdir build )
 cd build
-cmake -G "Visual Studio 15 2017 Win64" ..
+cmake -G "Visual Studio 15 2017 Win64" ..\src
 cd ..
--- res/config/gameconfig.lua
+++ res/config/gameconfig.lua
@@ -6,18 +6,9 @@ SPAWN_POINT = { 23, 234 }
 BOY_SURV = "man1"
 -- The "Girl" LG sprite name
 GIRL_SURV = "wmn2"
+
 -- The amount of experience required per level increase
 EXPERIENCE_SCALE = 1.33
--- The amount of core stat increase per level (which stats varies with class)
-CORESTAT_SCALE = 1.11
--- The amount of non-core stat increase per level (which stats varies with class)
-OTHERSTAT_SCALE = 1.001 
--- The increase in mob stats (all of its stats) per level
-MOB_STAT_SCALE = 1.13
--- base level (level=1) for core stats (which stats varies according to class)
-CORE_STATS = 5
--- base level (level=1) for non-core stats (which vary according to class)
-OTHER_STATS = 5
 -- the amount of exp to increase from level 1 to level 2
 BASE_EXP = 250
 -- the base experience value of a non-elite level 1 mob
@@ -26,6 +17,23 @@ BASE_MOB_EXP = 10
 BASE_MOB_EXP_SCALE = 1.25
 -- the experience penalty for killing lower level mobs per level difference
 MOB_EXP_PENALTY = 0.8
+
+-- The amount of core stat increase per level (which stats varies with class)
+CORESTAT_SCALE = 1.11
+-- The amount of non-core stat increase per level (which stats varies with class)
+OTHERSTAT_SCALE = 1.01 
+
+-- The increase in mob stats (all of its stats) per level
+MOB_STAT_SCALE = 1.13
+-- base level (level=1) for core stats (which stats varies according to class)
+CORE_STATS = 5
+-- base level (level=1) for non-core stats (which vary according to class)
+OTHER_STATS = 4
+
+-- lucky survivalist gets 2 AP stats as well as health
+SURVIVALIST_CORE_STATS = {
+    "Strength", "Agility", "Vitality"
+}
 
 -- Entries for all possible inventory items -----------------------------------
 -------------------------------------------------------------------------------
